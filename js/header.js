fea78b4af7d5afea23de420ce87a4314d43bd1c1
dyf <1092982458@qq.com>
2017-03-08 23:09:21 +0800

    project commit

---
+++ js/header.js
@@ -0,0 +1,4 @@
+document.write('<nav><div id="nav"><a href="index.html" class="lf"><img src="images/logo_03.png" alt=""></a><ul class="rt"><li><a href="index.html">首页</a></li><li><a href="productcenter.html">产品中心</a><ol>123</ol></li><li><a href="customservice.html">定制服务</a></li><li><a href="SoftwareSolution.html">软件解决方法</a></li><li><a href="marketsolutions.html">市场解决方法</a></li><li><a href="serve.html">支持服务</a></li><li><a href="about.html">关于我们</a></li><li><a href="news1.html">新闻资讯</a></li></ul></div></nav><div id="banner"><!--轮播图--><ul id="banner_img"></ul><!--轮播底部按钮--><div id="banner_an"></div><!--轮播左右箭头--><span class="banner_left arrow"><img src="images/jiao_03.png" alt=""></span><span class="banner_right arrow"><img src="images/jiao_05.png" alt=""></span></div>');
+fIndexTop()
+fClick($('#nav ol li'))
+oBanner.init();
